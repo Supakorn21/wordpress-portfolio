abfd5533cf03cb8fde3b415d89bdc23578e8ab0b
Supakorn Charoen <supakorncharoen@gmail.com>
2023-02-19 17:02:08 +0700

    [02-convering] - Loop over all the blogs posts on the front page

--- wp-content/themes/my-portfolio/functions.php
+++ wp-content/themes/my-portfolio/functions.php
@@ -2,5 +2,6 @@
 function mytheme_post_thumbnails()
 {
     add_theme_support('post-thumbnails');
+    add_post_type_support('page', 'excerpt');
 }
 add_action('after_setup_theme', 'mytheme_post_thumbnails');
